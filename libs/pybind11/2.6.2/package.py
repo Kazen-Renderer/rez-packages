f35489716182d5efc8fc9c18270b1288177813d2
joeyskeys <joey.skeys@gmail.com>
2021-06-24 14:18:23 +0800

    add pybind11

---
+++ libs/pybind11/2.6.2/package.py
@@ -0,0 +1,42 @@
+import platform
+
+name = "pybind11"
+
+version = "2.6.2"
+
+authors = [
+    "Wenzel Jacob"
+]
+
+description = \
+    """
+    pybind11 is a lightweight header-only library that exposes C++ types
+    in Python and vice versa, mainly to create Python bindings of existing
+    C++ code.
+    """
+
+build_requires = [
+    "cmake"
+]
+
+requires = [
+    "python-3.7.10",
+    "boost-1.73",
+    "zlib-1.2.11"
+]
+
+variants = []
+
+if platform.system() == "Darwin":
+    variants.append(["platform-osx", "arch-x86-64"])
+elif platform.system() == "Linux":
+    variants.append(["platform-linux", "arch-x86_64"])
+
+uuid = "libs.pybind11"
+
+
+def commands():
+    env.CMAKE_MODULE_PATH.append("{root}/cmake")
+
+    if building:
+        env.PKG_CONFIG_PATH.append("{root}/lib/pkgconfig")
