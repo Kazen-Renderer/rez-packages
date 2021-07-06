6151bbac3e1b6becec8ae239dad845fefce264ac
joeyskeys <joey.skeys@gmail.com>
2021-07-06 12:50:43 +0800

    add ocio

---
+++ libs/ocio/2.0.1/package.py
@@ -0,0 +1,44 @@
+import platform
+
+name = "ocio"
+
+version = "2.0.1"
+
+authors = [
+    "Sony Pictures Imageworks team"
+]
+
+description = \
+    """
+    A complete color management solution geared towards motion picture
+    production with an emphasis on visual effects and computer animation.
+    """
+
+build_requires = [
+    "cmake"
+]
+
+requires = [
+    "openexr-2.4.3",
+    "expat-2.2.8",
+    "yamlcpp-0.6.3",
+    "pystring-1.1.3",
+    "pybind11-2.6.2",
+    "python-3.7.10",
+]
+
+variants = []
+
+if platform.system() == "Darwin":
+    variants.append(["platform-osx", "arch-x86-64"])
+elif platform.system() == "Linux":
+    variants.append(["platform-linux", "arch-x86_64"])
+
+uuid = "libs.ocio"
+
+
+def commands():
+    env.CMAKE_MODULE_PATH.append("{root}/cmake")
+
+    if building:
+        env.PKG_CONFIG_PATH.append("{root}/lib/pkgconfig")
