5d84142258f6e19ec611735857c3f0b9dc17fc31
KazenZhong <tcx19900712@163.com>
2021-07-09 16:04:15 +0800

    cmake find_packages support for partial libs

--- libs/oiio/2.2.15/package.py
+++ libs/oiio/2.2.15/package.py
@@ -42,7 +42,7 @@ uuid = "libs.oiio"
 
 
 def commands():
-    env.CMAKE_MODULE_PATH.append("{root}/cmake")
+    env.CMAKE_PREFIX_PATH.append("{root}/lib/cmake/OpenImageIO")
 
     if building:
         env.PKG_CONFIG_PATH.append("{root}/lib/pkgconfig")
