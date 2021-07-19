c04be594d92f37d7cdce8cd652565da7c22f60c8
joeyskeys <joey.skeys@gmail.com>
2021-07-19 21:44:59 +0800

    add osl and pugixml

---
+++ libs/pugixml/1.11.4/package.py
@@ -0,0 +1,37 @@
+import platform
+
+name = "pugixml"
+
+version = "1.11.4"
+
+authors = [
+    "Arseny Kapoulkine"
+]
+
+description = \
+    """
+    pugixml is a light-weight C++ XML processing library.
+    """
+
+build_requires = [
+    "cmake"
+]
+
+requires = []
+
+variants = []
+
+if platform.system() == "Darwin":
+    variants.append(["platform-osx", "arch-x86_64"])
+elif platform.system() == "Linux":
+    variants.append(["platform-linux", "arch-x86_64"])
+
+
+uuid = "libs.pugixml"
+
+
+def commands():
+    env.CMAKE_MODULE_PATH.append("{root}/cmake")
+
+    if building:
+        env.PKG_CONFIG_PATH.append("{root}/lib/pkgconfig")
