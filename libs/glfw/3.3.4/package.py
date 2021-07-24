aacf0d66d74549cedcc56cfb28c981fcf4884cb0
钟凌霄-Tony Zhong <tcx19900712@163.com>
2021-07-24 17:47:18 +0800

    glfw & custom dev suites (#8)
    * glfw & suite packages
    * doc init

---
+++ libs/glfw/3.3.4/package.py
@@ -0,0 +1,38 @@
+import platform
+
+name = "glfw"
+
+version = "3.3.4"
+
+authors = [
+    "Copyright © 2002-2006 Marcus Geelnard",
+    "Copyright © 2006-2019 Camilla Löwy"
+]
+
+description = \
+    """
+    GLFW is an Open Source, multi-platform library for OpenGL, 
+    OpenGL ES and Vulkan development on the desktop.
+    """
+
+build_requires = [
+    "cmake"
+]
+
+requires = []
+
+variants = []
+
+if platform.system() == "Darwin":
+    variants.append(["platform-osx", "arch-x86-64"])
+elif platform.system() == "Linux":
+    variants.append(["platform-linux", "arch-x86_64"])
+
+uuid = "libs.glfw"
+
+
+def commands():
+    env.CMAKE_PREFIX_PATH.append("{root}/lib/cmake/glfw3")
+
+    if building:
+        env.PKG_CONFIG_PATH.append("{root}/lib/pkgconfig")
