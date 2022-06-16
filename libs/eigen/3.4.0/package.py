35d6a2e8826d4d9bb967cdd50afe712b0b1e79ee
joeyskeys <joey.skeys@gmail.com>
2022-06-16 11:11:27 +0800

    add eigen

---
+++ libs/eigen/3.4.0/package.py
@@ -0,0 +1,37 @@
+import platform
+
+name = "eigen"
+
+version = "3.4.0"
+
+authors = [
+    "Benoît Jacob",
+    "Gaël Guennebaud"
+]
+
+description = \
+    """
+    Eigen is a C++ template library for linear algebra: matrices, vectors,
+    numerical solvers, and related algorithms.
+    """
+
+build_requires = []
+
+requires = []
+
+variants = []
+
+if platform.system() == "Darwin":
+    variants.append(["platform-osx", "arch-x86_64"])
+elif platform.system() == "Linux":
+    variants.append(["platform-linux", "arch-x86_64"])
+
+uuid = "libs.eigen"
+
+
+def commands():
+    env.LD_LIBRARY_PATH.append("{root}/lib")
+    env.CMAKE_PREFIX_PATH.append("{root}/share/eigen3/cmake")
+
+    if building:
+        env.PKG_CONFIG_PATH.append("{root}/lib/pkgconfig")
