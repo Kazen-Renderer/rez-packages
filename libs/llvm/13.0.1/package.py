3dd3aac119f39df79eabc90a3455c5eb56d71c7b
joeyskeys <joey.skeys@gmail.com>
2022-12-05 17:53:22 +0800

    add llvm

---
+++ libs/llvm/13.0.1/package.py
@@ -0,0 +1,42 @@
+import platform
+
+name = "llvm"
+
+version = "13.0.1"
+
+authors = [
+    "Vikram Adve",
+    "Chris Lattner",
+    "LLVM Developer Group"
+]
+
+description = \
+    """
+    The LLVM Project is a collection of modular and reusable compiler and toolchain
+    technologies. Despite its name, LLVM has little to do with traditional virtual
+    machines. The name "LLVM" itself is not an acronym; it is the full name of the
+    project.
+    """
+
+build_requires = [
+    "cmake"
+]
+
+requires = []
+
+variants = []
+
+if platform.system() == "Darwin":
+    variants.append(["platform-osx", "arch-x86_64"])
+elif platform.system() == "Linux":
+    variants.append(["platform-linux", "arch-x86_64"])
+
+uuid = "libs.llvm"
+
+
+def commands():
+    env.LD_LIBRARY_PATH.append("{root}/lib")
+    env.CMAKE_PREFIX_PATH.append("{root}")
+
+    if building:
+        env.PKG_CONFIG_PATH.append("{root}/lib/pkgconfig")
